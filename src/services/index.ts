64db9de963ae2f1b3e4bc5542edb8de7ed1bfbda
xjh22222228 <xjh22222228@gmail.com>
2021-03-28 22:31:48 +0800

    fix: api auth

--- src/services/index.ts
+++ src/services/index.ts
@@ -180,7 +180,7 @@ export function getAccessToken(code: string) {
   return get('/api/oauth', {
     baseURL: process.env.NODE_ENV === 'development'
       ? 'http://localhost:7001'
-      : 'https://github-oauth-opal.vercel.app/api/oauth',
+      : 'https://github-oauth-opal.vercel.app',
     params: {
       code
     }
