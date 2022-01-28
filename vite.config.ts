025eef7251df3f01233492d735cf10f1559507bc
xjh22222228 <xjh22222228@gmail.com>
2022-01-28 11:42:56 +0800

    fix: reactivityTransform

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    script: {
+      reactivityTransform: true
+    }
+  })],
 
   resolve: {
     alias: [
